8b645fffb394a4f22d1556e82c1a3dab20ee8cc9
Eliabe dos Santos Oliveira <72847444+eliabedossantos@users.noreply.github.com>
2024-05-17 15:02:10 -0300

    u

--- src/components/AppContainer/AppContainer.styles.ts
+++ src/components/AppContainer/AppContainer.styles.ts
@@ -5,7 +5,7 @@ export const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-color: ${colors.primary.darkBlue};
-    padding: 2rem;
+    padding: 3rem;
 `;
 
 export const Main = styled.div`
